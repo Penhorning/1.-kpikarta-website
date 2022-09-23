7f74ec1c1af010227779f0bddc4cce3d5d519c2d
sachin.kumar <sachin.kumar@otssolutions.com>
2022-09-23 15:39:44 +0530

    update dev env file

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://dev-api.kpikarta.com'
+  // API_URL: 'https://dev-api.kpikarta.com'
+  API_URL: 'http://159.89.234.66:3340'
 };
