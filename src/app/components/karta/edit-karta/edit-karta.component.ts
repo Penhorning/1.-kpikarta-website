dcccd99cbfb7c3a36191c04e4c2874d103bcf74f
Tarun.Kethwalia <tarun.kethwalia@otssolutions.com>
2022-11-24 11:18:12 +0530

    Fixed undo redo phase issue

--- src/app/components/karta/edit-karta/edit-karta.component.ts
+++ src/app/components/karta/edit-karta/edit-karta.component.ts
@@ -1503,10 +1503,15 @@ export class EditKartaComponent implements OnInit {
                 if(x.data.data){
                   this.getRemovableNodeId = x.data.data.kartaNodeId;
                   this.returnChildNode(this.karta.node);
-                  this.D3SVG.updateRemovedNode(this.getRemovableNode);
-                  this.setKartaDimension();
-                  this.getRemovableNode = null;
-                  this.getRemovableNodeId = "";
+                  this._kartaService.getNode(this.getRemovableNode.parentId).subscribe((kartaNode: any) => {
+                    this.D3SVG.updateRemovedNode(this.getRemovableNode);
+                    let phase = this.phases[this.phaseIndex(kartaNode.phaseId)];
+                    kartaNode.phase = phase;
+                    this.setKartaDimension();
+                    this.updateNodeProperties(kartaNode);
+                    this.getRemovableNode = null;
+                    this.getRemovableNodeId = "";
+                  });
                 }
                 break;
               case "node_updated":
@@ -1599,10 +1604,15 @@ export class EditKartaComponent implements OnInit {
                 if(x.data.data){
                   this.getRemovableNodeId = x.data.data.kartaNodeId;
                   this.returnChildNode(this.karta.node);
-                  this.D3SVG.updateRemovedNode(this.getRemovableNode);
-                  this.setKartaDimension();
-                  this.getRemovableNode = null;
-                  this.getRemovableNodeId = "";
+                  this._kartaService.getNode(this.getRemovableNode.parentId).subscribe((kartaNode: any) => {
+                    this.D3SVG.updateRemovedNode(this.getRemovableNode);
+                    let phase = this.phases[this.phaseIndex(kartaNode.phaseId)];
+                    kartaNode.phase = phase;
+                    this.setKartaDimension();
+                    this.updateNodeProperties(kartaNode);
+                    this.getRemovableNode = null;
+                    this.getRemovableNodeId = "";
+                  });
                 }
                 break;
             }
